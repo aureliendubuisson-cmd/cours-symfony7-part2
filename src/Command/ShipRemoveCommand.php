fae17b6c5566ddcb79a361aeda7b1dd2b6cb9c22
aurelien <aurelien.dubuisson@sciencespo.fr>
2026-02-10 10:53:43 +0100

    feat:ShipRemoveCommand

--- src/Command/ShipRemoveCommand.php
+++ src/Command/ShipRemoveCommand.php
@@ -8,7 +8,6 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -21,33 +20,35 @@ class ShipRemoveCommand extends Command
     public function __construct(
         private StarshipRepository $shipRepo,
         private EntityManagerInterface $em,
-    )
-    {
+    ) {
         parent::__construct();
     }
 
     protected function configure(): void
     {
         $this
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+            ->addArgument('slug', InputArgument::REQUIRED, 'The slug of the starship')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
+        $slug = $input->getArgument('slug');
+        $ship = $this->shipRepo->findOneBy(['slug' => $slug]);
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
+        if (!$ship) {
+            $io->error('Starship not found.');
+
+            return Command::FAILURE;
         }
 
-        if ($input->getOption('option1')) {
-            // ...
-        }
+        $io->comment(sprintf('Removing starship: %s', $ship->getName()));
 
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $this->em->remove($ship);
+        $this->em->flush();
+
+        $io->success('Starship removed.');
 
         return Command::SUCCESS;
     }
